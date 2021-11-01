0ba6fe6a9f94200bac70c9a14fc7c0cd283bdcd8
linconviana <linconviana@gmail.com>
2021-11-01 13:45:54 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
+import NavBar from 'components/Navbar';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
+    <>
+    <NavBar />
     <div className="text-primary">
-      <h1>Bem viando ao React Spring</h1>
+      <div className="container">
+
+        <h1>Bem viando ao React Spring</h1>
+        <DataTable />
+
+      </div>
     </div>
+    <Footer />
+    </>
   );
 }
 
