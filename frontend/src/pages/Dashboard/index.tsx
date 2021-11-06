cd8560c44e87223673489b0310c0fffa1b5c6cb0
linconviana <linconviana@gmail.com>
2021-11-06 02:00:27 -0300

    Pagination

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -6,31 +6,32 @@ import NavBar from "components/Navbar";
 
 const Dashboard = () => {
     return (
-        <>
-    <NavBar />
-    <div className="container">
+    <>
+        <NavBar />
+        <div className="container">
 
-      <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
-      <div className="row px-3">
-        <div className="col-sm-6">
-          <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
-          <BarChart />
+        <div className="row px-3">
+            <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
+            <BarChart />
+            </div>
+            <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas Vendas</h5>
+            <DonutChart />
+            </div>
         </div>
-        <div className="col-sm-6">
-        <h5 className="text-center text-secondary">Todas Vendas</h5>
-          <DonutChart />
+
+        <div className="py-3">
+            <h2 className="text-primary">Todas Vendas</h2>
         </div>
-      </div>
 
-      <div className="py-3">
-        <h2 className="text-primary">Todas Vendas</h2>
-      </div>
+        <DataTable />
 
-      <DataTable />
+        </div>
 
-    </div>
-    <Footer />
+        <Footer />
     </>
     )
 }
