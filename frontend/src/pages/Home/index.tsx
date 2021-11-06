32bdb429ea7153cf3fe977e3fd7e79d6de674770
linconviana <linconviana@gmail.com>
2021-11-05 23:40:57 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,7 +1,21 @@
+import Footer from "components/Footer";
+import NavBar from "components/Navbar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
         <>
-        <h1>Home</h1>
+        <NavBar />
+        <div className="container mt-5">
+            <div className="jumbotron">
+                <h1 className="display-4">DSVendas</h1>
+                <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                <hr/>
+                <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                <Link className="btn btn-primary btn-large" to="/dashboard">Acessar dashborad</Link>
+            </div>
+        </div>
+        <Footer />
         </>
     )
 }
